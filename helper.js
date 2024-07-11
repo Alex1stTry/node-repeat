a64c43737cc4536414475b3954ac9d7baf78adbe
Malyshev Oleksndr <malyshevalex1994@gmail.com>
2024-07-11 18:45:57 +0300

    something is wrong

--- helper.js
+++ helper.js
@@ -1,13 +1,9 @@
-const {} = require('./users.json')
 const fs = require('node:fs/promises')
 const path = require("node:path");
 
 let users = []
-const pathToFile = path.join(__dirname, 'users.json')
 
-const writer = async (user) => {
-    await fs.writeFile(pathToFile, JSON.stringify(user))
-}
+const pathToFile = path.join(__dirname, 'users.json')
 
 const reader = async () => {
     const data = await fs.readFile(pathToFile, "utf-8");
@@ -15,10 +11,18 @@ const reader = async () => {
     return users
 }
 
+const writer = async () => {
+    await fs.writeFile(pathToFile, JSON.stringify(users, null))
+}
 
-
+const addUser = async (user) => {
+    const users = await readUsers();
+    users.push(user);
+    await writeUsers(users);
+};
 
 module.exports = {
     writer,
-    reader
+    reader,
+    addUser
 }
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const { writer, reader} = require("./helper");
-
+const {writer, reader} = require("./helper");
 
 
 const app = express();
@@ -9,13 +8,13 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
-    res.send('Hello')
+
 })
 
 app.get('/users', async (req, res) => {
     try {
-       const users = await reader();
-
+        const users = await reader();
+        res.json(users)
     } catch (e) {
         res.status(400).json(e.message)
     }
@@ -31,13 +30,28 @@ app.post('/users', async (req, res) => {
             email,
             password
         }
-        console.log(newUser)
+        const oldUser = users.find(user => user.email === email)
+        if (oldUser) {
+            res.status(409).json("User already exist")
+        }
+            await writer(newUser)
+            res.json(newUser)
+    } catch (e) {
+        res.status(400).json(e.message)
+    }
+})
+app.delete('/users/:id', async (req, res) => {
+    try {
+        const users = await reader()
+        const userId = +req.params.id;
         console.log(users)
-        // const oldUser = users.find(user => user.email === email)
-        // if (oldUser){
-        //     res.status(409).json("User already exist")
-        // }
-        // await writer(newUser)
+        const userIndex = users.findIndex(user => user.id === userId)
+        if (userIndex === -1) {
+            return res.status(404).json('User not found')
+        }
+        users.splice(userIndex, 1)
+        await writer()
+        res.status(204).end()
     } catch (e) {
         res.status(400).json(e.message)
     }
