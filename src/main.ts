75806ff235ffba0418db5cd870410c536da94882
Malyshev Oleksndr <malyshevalex1994@gmail.com>
2024-08-12 16:47:56 +0300

    swagger start

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import upload from "express-fileupload";
+import rateLimit from "express-rate-limit";
 import * as mongoose from "mongoose";
+import swaggerUi from "swagger-ui-express";
 
+import swaggerDoc from "../docs/swagger.json";
 import { Configs } from "./configs/configs";
 import { ApiError } from "./errors/api-error";
 import { authRouter } from "./routers/auth.router";
@@ -12,7 +16,28 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(upload());
-
+app.use(
+  rateLimit({
+    windowMs: 60 * 1000,
+    limit: 5,
+    message: "max 5 requests,wait a minute",
+  }),
+);
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: [
+      "Authorization",
+      "Content-Type",
+      "Origin",
+      "Access-Control-Allow-Origin",
+    ],
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+  }),
+);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use("/users", userRouter);
 app.use("/auth", authRouter);
 
